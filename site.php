af9ecc5493a58895af93c9330ad1147f769075cc
joaoDavidGB <joaobaiao12@gmail.com>
2015-11-27 01:12:07 +0000

    alterações drasticas

--- site.php
+++ site.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
- 
+
 <html>
   <head>
 	<meta charset="UTF-8">
@@ -9,46 +9,26 @@
 
 	<body bgcolor="white">
 		<div id="header">
-			<h2>Alpha v0.005</h2>
-			<h1>Events</h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<li><a href="accounts/registar.php">Register</a></li>
-				<li><a href="events/criarEvento.html">Create event</a></li>
-			</ul>
-			<div id="loginMenu" style="display: inline;">
-				<?php
-					if(date("H") < 23){
-		   				echo'<form id="loginForm" method="post" style="display: inline;" action="accounts/login.php">
-						Username/Password: <input type="text" name="username">
-						<input type="password" name="pw">
-						<input type="submit" value="Login">';
-					}
-					else{
-						echo '<div id=loged>LogedIn</div>';
-					}
-				?>
+			<div id="data">
+			<?
+				//date_default_timezone_set('GMT');
+				echo date('d\/m\/Y')."<br>".date('l H:i e');
+			?>
 			</div>
 		</div>
-		<div id="FirstBoxes"> 
-			
-			<div id="procurar_eventos">
-				<h1> Search Events </h1>
-				<form action="events/pesquisaEventos.php">
-				Name:<br><input type="text" name="nomeEvento"><br>
-				Local:<br><input type="text" name="local"><br>
-				Date:<br><input list="data" name="data">
-					<datalist id="data">
-						<option value="Today">
-						<option value="Tomorrow">
-						<option value="This week">
-						<option value="This month">
-						<option value="Ever">
-					</datalist>
-				<br><br><input type="submit" value="Search"><br><br>
-			</form>
-			</div>
+
+		 <div id="eventList">
+		 	<?
+		 		echo "event1 <br><br><br><br><br><br><br>";
+		 		echo "event2 <br><br><br><br><br><br><br>";
+		 		echo "event3 <br><br><br><br><br><br><br>";
+		 		echo "event4 <br><br><br><br><br><br><br>";
+		 		echo "event5 <br><br><br><br><br><br><br>";
+		 		echo "event6 <br><br><br><br><br><br><br>";
+		 		echo "event7 <br><br><br><br><br><br><br>";
+		 		echo "event8 <br><br><br><br><br><br><br>";
+		 		echo "event9 <br><br><br><br><br><br><br>";
+		 	?>
 		</div>
 	</body>
 </html>
