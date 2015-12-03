a433c0f75c2d7045a4da42581586f786d94a70bd
F0lha <pedrovieiracastro@hotmail.com>
2015-12-03 14:23:53 +0000

    ola

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 	session_start();
 
-
-
 $redirect = isset($_GET['redirect']) ? $_GET['redirect'] : 'site';
 
 if (!isset($_SESSION['username'])){
 		$_SESSION['username'] = null;
-		$redirect = home;
+		$redirect = 'home';
 }
 else if($redirect === 'home')
 	$redirect = 'site';
